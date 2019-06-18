76293a9a693aabca7812a5e74627cd8458d64ee3
Einscrew <manel.rodrigo.reis@gmail.com>
2019-06-18 11:08:51 +0100

    Time feature added

--- request.sh
+++ request.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+
+DATE=$(date +"%Y-%m-%d" --date="$(echo $@)")
+
 curl -s 'https://www.sas.ulisboa.pt/index.php' -c cookies > /dev/null
 
 curl -s 'https://www.sas.ulisboa.pt/index.php' \
 	-b cookies \
-	--data 'opt=4%3B4H1&chvP=116%3B'$(date +"%Y-%m-%d")'%3B1&dia=&other=&searchbox=' \
-	--compressed > request.html
+	--data 'opt=4%3B4H1&chvP=116%3B'"$DATE"'%3B1&dia=&other=&searchbox=' > request.html
 
 grep -Pzo 'Menu Vegetariano</h2>(.*)*\n(.*)<div(.*)*\n(.*)*\n(.*)<h3(.*)*\n' request.html | \
 	sed -e 's/<[^>]*>//g' | \
